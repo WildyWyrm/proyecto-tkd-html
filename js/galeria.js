aa30f40312bc0d001aed6d8b9e0d9e6c723bcb71
Brian <brianbasabee@gmail.com>
2025-07-18 14:08:16 -0300

    feature:Agregado de imagekit

--- js/galeria.js
+++ js/galeria.js
@@ -1,57 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
   const galleryContainer = document.getElementById("gallery");
   const loadMoreButton = document.getElementById("loadMore");
-  const numImages = 174; // Número total de imágenes
-  const imagesPerLoad = 3; // Número de imágenes a cargar por clic
-  let currentIndex = 1; // Índice de la imagen actual
+  const numImages = 174; // Total de imágenes que subiste
+  const imagesPerLoad = 3; // Cantidad de imágenes a cargar por clic
+  let currentIndex = 1; // Empezamos en la imagen 1
+
+  const baseURL = "https://ik.imagekit.io/m2lli2rcl/Fotos/";
 
   function loadImages() {
-      const fragment = document.createDocumentFragment(); // Usar un fragmento para mejorar el rendimiento
+      const fragment = document.createDocumentFragment();
 
       for (let i = currentIndex; i < currentIndex + imagesPerLoad && i <= numImages; i++) {
           const link = document.createElement("a");
           link.setAttribute("data-lightbox", "images");
           link.setAttribute("rel", "lightbox");
           link.setAttribute("data-title", "Taekwon-do ITF - Team Basabe");
-          link.setAttribute("href", `img/Fotos/${i}.webp`);
+          link.setAttribute("href", `${baseURL}${i}.webp`);
 
           const image = document.createElement("img");
-          image.src = `img/Fotos/${i}.webp`; // Cambiado de data-src a src
-          image.alt = ""; 
-          image.loading = "lazy"; // Mantener la carga diferida
+          image.src = `${baseURL}${i}.webp`;
+          image.alt = `Imagen Taekwon-do ITF número ${i}`;
+          image.loading = "lazy";
 
           link.appendChild(image);
           fragment.appendChild(link);
       }
 
       galleryContainer.appendChild(fragment);
-      currentIndex += imagesPerLoad; // Actualiza el índice actual
+      currentIndex += imagesPerLoad;
 
-      // Si ya se han cargado todas las imágenes, oculta el botón "Cargar más"
       if (currentIndex > numImages) {
           loadMoreButton.style.display = 'none';
       }
   }
 
-  // Cargar las primeras imágenes al inicio
   loadImages();
 
-  // Configurar el evento de clic en el botón "Cargar más"
   loadMoreButton.addEventListener("click", loadImages);
-});
-
-/* Funcion flecha de retorno */
-window.addEventListener("scroll", function() {
-  let arrow = document.getElementById("arrow");
-  if (window.scrollY > 300) { // Cambia 300 por la posición en la que deseo mostrar la flecha
-      arrow.classList.add("visible");
-      arrow.classList.remove("hidden");
-  } else {
-      arrow.classList.add("hidden");
-      arrow.classList.remove("visible");
-  }
-});
-
-document.getElementById("arrow").addEventListener("click", function() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
 });
